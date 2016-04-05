d3d20cd483b3e14b428af0c47abc4b050564d234
Laarni Astrid <laarni.astrid@gmail.com>
2016-04-05 11:30:18 -0600

    added base

---
+++ index.js
@@ -0,0 +1,41 @@
+// ---------- initial vars for app ----------
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    port = process.env.PORT || 8000,
+    connectPath = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/brds',
+    app = express(),
+    keys = require('./keys.js');
+
+
+// ---------- .use for app ----------
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '../public/index.html'));  // static part of site to connect to
+
+
+// ---------- connection to mongoose/heroku ----------
+mongoose.set('debug', true);
+mongoose.connect(connectPath, function(err) {
+  if (err) {
+    console.log(err);
+  }
+});
+mongoose.connection.once('open', function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('connected to MongoDb');
+  }
+});
+
+
+
+
+// ---------- app.listen ----------
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('listening on port ', port);
+  }
+});
