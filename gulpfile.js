2045c24c8397a670167395f797509f74fadc9785
Laarni Astrid <laarni.astrid@gmail.com>
2016-04-10 21:36:29 -0600

    re-added dirs scripts/styles to public

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp'),
+    // stylus = require('gulp-stylus'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    // uglify = require('gulp-uglify'),
+    uglifycss = require('gulp-uglifycss'),
+    ngAnnotate = require('gulp-ng-annotate'),
+    watcher = gulp.watch(['./main/client/src/**/*.js', './main/client/styles/*.styl', './main/client/features/**/*.html'], ['default']);
+
+watcher.on('change', function(event) {
+    console.log('File ' + event.path + ' was ' + event.type + ' at ' + new Date() + ' , running tasks...');
+});
+
+gulp.task('sass', function() { // will double check out styles|sass wording|npm install
+    return gulp.src('./main/styles/*.scss')
+        // .pipe(stylus())
+        .pipe(sass())
+        .pipe(uglifycss())
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest('./public/styles'))
+});
+
+gulp.task('javascript', function() {
+    gulp.src('./main/**/*.js')
+        .pipe(ngAnnotate())
+        // .pipe(uglify())
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('./public/scripts'))
+});
+
+gulp.task('html', function() {
+    gulp.src('./main/**/*.html')
+        // .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('./public/html/'))
+});
+
+gulp.task('default', ['styles', 'javascript', 'html']);
