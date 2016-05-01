e60fa3020f85f32c5ef9b871ba2a1ebdf1cd0a35
Laarni Astrid <laarni.astrid@gmail.com>
2016-05-01 02:03:23 -0600

    updating app.js templateurl's to 'views' instead of 'tmpl'

--- main/app.js
+++ main/app.js
@@ -5,43 +5,43 @@ angular.module('myApp', ['ui.router'])
   $stateProvider
   .state('home', {
     url: '/home',
-    templateUrl: './html/home/homeTmpl.html'
+    templateUrl: './html/home/homeView.html'
   })
   .state('about', {
     url: '/about',
-    templateUrl: './html/about/aboutTmpl.html'
+    templateUrl: './html/about/aboutView.html'
   })
   .state('contact', {
     url: '/contact',
-    templateUrl: './html/contact/contactTmpl.html'
+    templateUrl: './html/contact/contactView.html'
   })
   .state('trafficking', {
     url: '/trafficking',
-    templateUrl: './html/trafficking/traffickingTmpl.html'
+    templateUrl: './html/trafficking/traffickingView.html'
   })
   .state('sellBrds', {
     url: '/sellBrds',
-    templateUrl: './html/sellBrds/sellBrdsTmpl.html'
+    templateUrl: './html/sellBrds/sellBrdsView.html'
   })
   .state('rentBrds', {
     url: '/rentBrds',
-    templateUrl: './html/rentBrds/rentBrdsTmpl.html'
+    templateUrl: './html/rentBrds/rentBrdsView.html'
   })
   .state('donateBrds', {
     url: '/donateBrds',
-    templateUrl: './html/donateBrds/donateBrdsTmpl.html'
+    templateUrl: './html/donateBrds/donateBrdsView.html'
   })
   .state('buyBrds', {
     url: '/buyBrds',
-    templateUrl: './html/buyBrds/buyBrdsTmpl.html'
+    templateUrl: './html/buyBrds/buyBrdsView.html'
   })
   .state('store', {
     url: '/store',
-    templateUrl: './html/store/storeTmpl.html'
+    templateUrl: './html/store/storeView.html'
   })
   .state('cart', {
     url: '/cart',
-    templateUrl: './html/cartTmpl.html'
+    templateUrl: './html/cartView.html'
   })
 
   $urlRouterProvider.otherwise('/home');
