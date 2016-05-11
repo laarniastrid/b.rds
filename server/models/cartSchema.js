33a9d99f14ef10ae838772ad3e22fd1717834697
Laarni Astrid <laarni.astrid@gmail.com>
2016-05-10 23:11:29 -0600

    added base cart schema info

--- server/models/cartSchema.js
+++ server/models/cartSchema.js
@@ -2,3 +2,19 @@
 
 let mongoose = require('mongoose'),
     Schema = mongoose.Schema;
+
+let cartSchema = new Schema({
+  products: [{
+    item: {
+      type: Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true
+    },
+    quantity: {
+      type: Number,
+      min: 1
+    }
+  }]
+})
+
+module.exports = mongoose.model('Cart', cartSchema);
