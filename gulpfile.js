09347b058c477e99f8921cfc798bfdb39647650a
Laarni Astrid <laarni.astrid@gmail.com>
2016-05-15 02:41:31 -0600

    trying to get ngcart working

--- gulpfile.js
+++ gulpfile.js
@@ -6,39 +6,44 @@ var gulp = require('gulp'),
     // uglify = require('gulp-uglify'),
     uglifycss = require('gulp-uglifycss'),
     ngAnnotate = require('gulp-ng-annotate'),
-    watcher = gulp.watch(['./main/**/*.js', './main/**/*.scss', './main/**/*.html'], ['default']);
+    watcher = gulp.watch(['./main/**/*.js', './main/**/*.scss', './main/**/*.html', './bower_components/ngCart/template'], ['default']);
 
 watcher.on('change', function(event) {
-    console.log('File ' + event.path + ' was ' + event.type + ' at ' + new Date() + ' , running tasks...');
+  console.log('File ' + event.path + ' was ' + event.type + ' at ' + new Date() + ' , running tasks...');
 });
 
 gulp.task('sass', function() { // will double check out styles|sass wording|npm install
-    return gulp.src('./main/**/*.scss')
-        // .pipe(stylus())
-        .pipe(sass())
-        // .pipe(uglifycss())
-        .pipe(concat('main.css'))
-        .pipe(gulp.dest('./public/styles'))
+  return gulp.src('./main/**/*.scss')
+    // .pipe(stylus())
+    .pipe(sass())
+    // .pipe(uglifycss())
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest('./public/styles'))
 });
 
 gulp.task('javascript', function() {
-    gulp.src(['./main/**/*.js'])
-        .pipe(ngAnnotate())
-        // .pipe(uglify())
-        // .pipe(addsrc('./animateDir.js'))
-        // .pipe(order([
-        //   'app.js',
-        //   'main/**/*.js',
-        //   'animateDir.js'
-        // ], { base: './' }))
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('./public/scripts'))
+  gulp.src(['./main/**/*.js'])
+    .pipe(ngAnnotate())
+    // .pipe(uglify())
+    // .pipe(addsrc('./animateDir.js'))
+    // .pipe(order([
+    //   'app.js',
+    //   'main/**/*.js',
+    //   'animateDir.js'
+    // ], { base: './' }))
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./public/scripts'))
 });
 
 gulp.task('html', function() {
-    gulp.src('./main/**/*.html')
-        // .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(gulp.dest('./public/html/'))
+  gulp.src('./main/**/*.html')
+    // .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('./public/html/'))
 });
 
-gulp.task('default', ['sass', 'javascript', 'html']);
+gulp.task('ngCartTmpl', function() {
+  gulp.src(['./public/bower_components/ngCart/template'])
+    .pipe(gulp.dest('./public/template'))
+})
+
+gulp.task('default', ['sass', 'javascript', 'html', 'ngCartTmpl']);
